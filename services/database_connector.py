a1aedb53e5e0def9a2067abed79916184f0633f1
Mohamed Rami Jebali <mohamedrami.jebali@enicar.ucar.tn>
2024-12-03 16:10:19 +0100

    Add env

--- services/database_connector.py
+++ services/database_connector.py
@@ -1,21 +1,19 @@
-import dotenv
+from dotenv import load_dotenv
+import os
 from sqlalchemy import create_engine, URL
 
+load_dotenv()
 
 
 def createDatabaseConnection() -> object:
     url = URL.create(
-            drivername='postgresql+psycopg2',
-            username='postgres',
-            password='',
-            host='localhost',
-            port='5432',
-            database='games_sales'
-            )
+        drivername="postgresql+psycopg2",
+        username=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+    )
 
     engine = create_engine(url)
     return engine
-
-
-
-
